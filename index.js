dae625fb2d9d6ea2edf25cb822c6667ea9140712
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-19 22:12:54 -0300

    alteração código html e js

---
+++ index.js
@@ -0,0 +1,12 @@
+// let inputSelecionado = document.querySelectorAll("[type='text']");
+let inputSelecionado = document.getElementsByTagName('input');
+
+console.log(inputSelecionado);
+
+// inputSelecionado.addEventListener('click', () =>{
+//     if(inputSelecionado.innerHTML == ''){
+//         alert("preeencher");
+//     }else if(!inputSelecionado.innerHTML == ''){
+//         alert("Uhuuuuu!!!!")
+//     }
+// })
